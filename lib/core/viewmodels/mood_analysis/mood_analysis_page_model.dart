ed63317705c04d96901d636c7bfd604d8f1cc644
raunak_k02 <raunakk728@gmail.com>
2021-06-13 19:16:12 +0530

    ptch: improves the camera preview

--- lib/core/viewmodels/mood_analysis/mood_analysis_page_model.dart
+++ lib/core/viewmodels/mood_analysis/mood_analysis_page_model.dart
@@ -1,29 +1,145 @@
-import 'dart:io';
-
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:maser/core/managers/camera_manager.dart';
 import 'package:stacked/stacked.dart';
 
-class MoodAnalysisPageModel extends BaseViewModel {
-  File image;
+class MoodAnalysisPageModel extends BaseViewModel with WidgetsBindingObserver {
+  XFile image;
+  FToast fToast = FToast();
   CameraController controller;
 
+  // Counting pointers (number of user fingers on screen)
+  int pointers = 0;
+  double minAvailableZoom = 1.0;
+  double maxAvailableZoom = 1.0;
+  double currentScale = 1.0;
+  double baseScale = 1.0;
+
+  //Initialize camera controller
   Future initCamController() async {
     this.setBusy(true);
-    controller = CameraController(cameras[0], ResolutionPreset.max);
+    WidgetsBinding.instance.addObserver(this);
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
     try {
       await controller.initialize();
-    } on CameraException {
-      throw error(CameraException);
+    } on CameraException catch (e) {
+      throw error(e);
     }
     this.setBusy(false);
   }
 
-  Future geImageFromCamera() async {}
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController cameraController = controller;
+
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      onNewCameraSelected(cameraController.description);
+    }
+  }
+
+  void onNewCameraSelected(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+    final CameraController cameraController = CameraController(
+      cameraDescription,
+      ResolutionPreset.medium,
+    );
+    controller = cameraController;
+
+    // If the controller is updated then update the UI.
+    cameraController.addListener(() {
+      if (!this.disposed) {
+        notifyListeners();
+      }
+      if (cameraController.value.hasError) {
+        print('Camera error ${cameraController.value.errorDescription}');
+      }
+    });
+
+    try {
+      await cameraController.initialize();
+      await Future.wait([
+        cameraController.getMaxZoomLevel().then((value) => maxAvailableZoom = value),
+        cameraController.getMinZoomLevel().then((value) => minAvailableZoom = value),
+      ]);
+    } on CameraException catch (e) {
+      Fluttertoast.showToast(msg: e.toString());
+    }
+
+    if (!this.disposed) {
+      notifyListeners();
+    }
+  }
+
+  /// Returns a suitable camera icon for [direction].
+  IconData getCameraLensIcon(CameraLensDirection direction) {
+    switch (direction) {
+      case CameraLensDirection.back:
+        return Icons.camera_rear;
+      case CameraLensDirection.front:
+        return Icons.camera_front;
+      case CameraLensDirection.external:
+        return Icons.camera;
+      default:
+        throw ArgumentError('Unknown lens direction');
+    }
+  }
+
+  //TODO: implement tensorflow logic
+
+  //Func to call to take pictures and save them in file
+  void onTakePictureButtonPressed() {
+    takePicture().then((XFile file) {
+      if (!this.disposed) {
+        image = file;
+        notifyListeners();
+        if (file != null)
+          Fluttertoast.showToast(
+            msg: 'Picture saved to ${file.path}',
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 2,
+          );
+      }
+    });
+  }
+
+  //Take picture from camera
+  Future<XFile> takePicture() async {
+    if (controller == null || !controller.value.isInitialized) {
+      Fluttertoast.showToast(
+        msg: 'Error: select a camera first.',
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 2,
+      );
+      return null;
+    }
+
+    if (controller.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+
+    try {
+      XFile file = await controller.takePicture();
+      return file;
+    } on CameraException catch (e) {
+      throw error(e);
+    }
+  }
 
   @override
   void dispose() {
     controller.dispose();
+    WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 }
--- lib/meta/views/mood_analysis/mood_analysis_page.dart
+++ lib/meta/views/mood_analysis/mood_analysis_page.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:maser/app/locator.dart';
+import 'package:maser/app/theme/app_colors.dart';
+import 'package:maser/core/managers/camera_manager.dart';
 import 'package:maser/core/viewmodels/mood_analysis/mood_analysis_page_model.dart';
 import 'package:maser/meta/widgets/custom_app_bar.dart';
 import 'package:stacked/stacked.dart';
@@ -21,8 +25,126 @@ class MoodAnalysisPage extends StatelessWidget {
           }
           return Scaffold(
             appBar: CustomAppBar(title: 'Mood Analysis'),
-            body: CameraPreview(model.controller),
+            body: Stack(
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: _cameraPreviewWidget(model),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      _cameraTogglesRowWidget(model),
+                      _thumbnailWidget(model),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+              ],
+            ),
+            floatingActionButton: _captureCameraButton(model),
           );
         });
+  }
+
+  Widget _cameraPreviewWidget(MoodAnalysisPageModel model) {
+    if (model.controller == null || !model.controller.value.isInitialized) {
+      return const Text(
+        'Tap a camera',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    } else {
+      return Listener(
+        onPointerDown: (_) => model.pointers++,
+        onPointerUp: (_) => model.pointers--,
+        child: CameraPreview(
+          model.controller,
+        ),
+      );
+    }
+  }
+
+  Widget _thumbnailWidget(MoodAnalysisPageModel model) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            model.image == null
+                ? Container()
+                : SizedBox(
+                    child: Image.file(File(model.image.path)),
+                    width: 64.0,
+                    height: 64.0,
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Display the control bar with buttons to take pictures and record videos.
+  Widget _captureCameraButton(MoodAnalysisPageModel model) {
+    final CameraController cameraController = model.controller;
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 80),
+      child: FloatingActionButton(
+        child: const Icon(Icons.camera_alt),
+        onPressed: cameraController != null &&
+                cameraController.value.isInitialized &&
+                !cameraController.value.isRecordingVideo
+            ? model.onTakePictureButtonPressed
+            : null,
+      ),
+    );
+  }
+
+  Widget _cameraTogglesRowWidget(MoodAnalysisPageModel model) {
+    final List<Widget> toggles = <Widget>[];
+
+    final onChanged = (CameraDescription description) {
+      if (description == null) {
+        return;
+      }
+
+      model.onNewCameraSelected(description);
+    };
+
+    if (cameras.isEmpty) {
+      return const Text('No camera found');
+    } else {
+      for (CameraDescription cameraDescription in cameras) {
+        toggles.add(
+          SizedBox(
+            width: 90.0,
+            child: RadioListTile<CameraDescription>(
+              title: Icon(
+                model.getCameraLensIcon(cameraDescription.lensDirection),
+                color: AppColors.slate_grey,
+              ),
+              activeColor: AppColors.grass,
+              groupValue: model.controller.description,
+              value: cameraDescription,
+              onChanged: model.controller != null && model.controller.value.isRecordingVideo
+                  ? null
+                  : onChanged,
+            ),
+          ),
+        );
+      }
+    }
+
+    return Row(children: toggles);
   }
 }
