ba0bb763bba60ba5f1d44cd72fc36d16106203d4
raunak_k02 <raunakk728@gmail.com>
2021-06-25 02:10:17 +0530

    reverts merge of PR #20

--- lib/app/chats/domain/repositories/chats_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import '../../../../core/error/failures.dart';
-import '../entities/chat_group.dart';
-
-abstract class ChatsRepository {
-  Future<Either<Failure, List<ChatGroup>>> getChatGroups();
-  Future<Either<Failure, void>> deleteChatGroup(String chatGroupId);
-  Future<Either<Failure, String>> createChatGroup(String mentorId);
-}
